86ed56e765b6b8a8b0fbaa6f10f36821af5f78d7
“Ali <ata@viabill.com>
2024-01-02 22:15:10 +0300

    removed space

--- lib/view/dog_list_page.dart
+++ lib/view/dog_list_page.dart
@@ -5,7 +5,6 @@ import 'package:dog_app/repository/dog_repository.dart';
 import 'package:dog_app/repository/models/dogs.dart';
 import 'package:dog_app/state/bloc/dog_bloc.dart';
 import 'package:dog_app/view/dog_details_page.dart';
-
 import 'package:dog_app/view/widgets/search_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
