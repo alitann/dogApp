7383fa028b726300553cb53035059d9fe15f72c9
“Ali <ata@viabill.com>
2024-01-03 10:07:01 +0300

    android fix

--- android/app/src/main/kotlin/com/example/dog_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dog_app/MainActivity.kt
@@ -7,8 +7,7 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
-
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
 
@@ -16,8 +15,6 @@ class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-        
-        // Diğer kodlar...
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
             .setMethodCallHandler { call, result ->
