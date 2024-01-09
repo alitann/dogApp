5e0bc64c7236685d1d1acfc474bc454a05c7968f
“Ali <ata@viabill.com>
2024-01-09 13:44:46 +0300

    error

--- lib/view/utils/tracker.dart
+++ lib/view/utils/tracker.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+
 abstract class Tracker {
-  static Future<void> onZoneError(Object error, StackTrace stackTrace) async {}
+  static Future<void> onZoneError(Object error, StackTrace stackTrace) async {
+    // Hata işleme fonksiyonu
+    debugPrint('Error: $error');
+    debugPrint('Stack trace: $stackTrace');
+  }
 }
